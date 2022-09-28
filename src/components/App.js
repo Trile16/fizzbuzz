acd79e5f03142b6dbb4d2ca6f8884fb6f2782aea
Tri <16trile@gmail.com>
2022-09-28 14:59:48 -0500

    fizzbuzz

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(props) {
+  return (
+    <>
+      <button>Increment</button>
+      <div>{"This is where you will output the answer"}</div>
+    </>
+  );
+}
+
+export default App;

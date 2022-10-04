4df43d169d537008279c08ca5900ad0ca9dc07fa
Tri <16trile@gmail.com>
2022-10-04 14:53:48 -0500

    fizzbuzz with prompt

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+// FizzBuzz!!!
+// Using React, write a component that has a button that increments up by one, starting at 0.
+// For each multiple of 3, print "Fizz" instead of the number.
+// For each multiple of 5, print "Buzz" instead of the number.
+// For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+
+import React, { useState } from "react";
 
 function App(props) {
   return (
     <>
-      <button>Increment</button>
-      <div>{"This is where you will output the answer"}</div>
+      <button onClick={evaluate}>Increment</button>
+      <div>{word}</div>
     </>
   );
 }
